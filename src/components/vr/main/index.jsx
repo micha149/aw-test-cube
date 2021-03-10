c7240ff67e694f821fb5bc6d2ee74cc251446f59
Michael van Engelshoven <michael@van-engelshoven.de>
2021-03-10 21:30:47 +0100

    Implement a simple cube map

--- src/components/vr/main/index.jsx
+++ src/components/vr/main/index.jsx
@@ -1,36 +1,22 @@
-import * as THREE from 'three'
-import React, { useEffect } from 'react'
-import { useLoader, useThree } from 'react-three-fiber';
-import { CubeTextureLoader } from 'three';
-
-
+import React from 'react'
+import { useCubeTexture } from '@react-three/drei';
+import { BackSide, ShaderLib, UniformsUtils } from 'three';
 
 const VRMain = React.memo(({ path }) => {
-    const addURLPrefix = (x) => `${path}${x}`;
-
-    const textures = [ 'px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png' ].map(addURLPrefix)
-    const [cubeMapTexture] = useLoader(CubeTextureLoader, [textures]);
-    
-    const {
-      scene
-    } = useThree();
-
-    useEffect(() => {
-        const previous = scene.background;
-        scene.background = cubeMapTexture;
-        
-        scene.background.minFilter = THREE.LinearFilter
-        scene.background.magFilter = THREE.LinearFilter
-
-        return () => {
-          scene.background = previous;
-        };
-    }, [cubeMapTexture, scene]);
+    const envMap = useCubeTexture(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'], { path })
 
     return (
-        <React.Fragment>
-            
-        </React.Fragment>
+        <mesh onClick={(e) => console.log('canvas', [e.point])}>
+            <boxBufferGeometry args={[500, 500, 500]} />
+            <shaderMaterial
+                fragmentShader={ShaderLib.cube.fragmentShader}
+                vertexShader={ShaderLib.cube.vertexShader}
+                uniforms={UniformsUtils.clone(ShaderLib.cube.uniforms)}
+                uniforms-envMap-value={envMap}
+                envMap={envMap}
+                side={BackSide}
+            />
+        </mesh>
     )
 
 })
